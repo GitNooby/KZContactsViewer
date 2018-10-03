401327a057f162fd24961ad9b8d90a315b502d1f
Kai Zou <kaizoum@kais-macbook-pro.local>
2018-10-02 19:26:12 -0700

    Initial Commit

---
+++ KZContactsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KZContactsViewer
+//
+//  Created by Kai Zou on 10/2/18.
+//  Copyright © 2018 Kai Zou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
